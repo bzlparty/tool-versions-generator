c1d6ac28fa907abe5012b069cd4f010f943e1a29
Marc Redemske <github@mgred.dev>
2023-06-23 03:03:24 +0200

    github: fix release script

--- .github/workflows/create_assets.sh
+++ .github/workflows/create_assets.sh
@@ -2,19 +2,22 @@
 
 version=$@
 
+function build_and_pack()
+{
+  GOOS=$1 GOARCH=$2 go build -v github.com/bzlparty/tool-versions-generator/cmd/tvg
+  if [[ "$1" == "windows" ]]; then
+    zip "tvg-${version}-${1}_${2}.zip" tvg.exe LICENSE README.md
+    rm tvg.exe
+  else
+    tar cvf "tvg-${version}-${1}_${2}.tar.gz" tvg LICENSE README.md
+    rm tvg
+  fi
+}
+
 echo "Create assets for $version"
 
-for os in darwin linux windows; do
-  for arch in amd64 386; do
-    echo "Build $os $arch"
-    GOOS=$o GOARCH=$a go build -v github.com/bzlparty/tool-versions-generator/cmd/tvg
-
-    if [[ "$os" == "windows" ]]; then
-      zip tvg-$version-$os_$arch.zip tvg.exe LICENSE README.md
-      rm tvg.exe
-    else
-      tar cvf tvg-$version-$os_$arch.tar.gz tvg LICENSE README.md
-      rm tvg
-    fi
-  done
-done
+build_and_pack linux amd64
+build_and_pack linux 386
+build_and_pack darwin amd64
+build_and_pack windows amd64
+build_and_pack windows 386
